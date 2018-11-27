501a8c477a4f63e59abfd681dc363b81375299bc
Mitchell Van Cleave <darealmvc@gmail.com>
2018-11-27 14:17:33 -0500

    file structure update 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
